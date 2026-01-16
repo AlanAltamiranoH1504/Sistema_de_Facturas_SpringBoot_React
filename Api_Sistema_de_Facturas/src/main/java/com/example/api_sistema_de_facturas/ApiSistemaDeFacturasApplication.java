c933b2c32f622650a0e91cc863ab11edf51f631f
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2026-01-16 17:42:14 -0600

    initial commit backend

---
+++ Api_Sistema_de_Facturas/src/main/java/com/example/api_sistema_de_facturas/ApiSistemaDeFacturasApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_sistema_de_facturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSistemaDeFacturasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSistemaDeFacturasApplication.class, args);
+    }
+
+}
